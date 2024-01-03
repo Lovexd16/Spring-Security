75bc74c3ac6fd884ebd932c6f75fc49efc42460d
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-01-03 17:46:40 +0100

    Security User details

--- springsecurityintro/src/main/java/com/intro/springsecurityintro/SecurityConfig.java
+++ springsecurityintro/src/main/java/com/intro/springsecurityintro/SecurityConfig.java
@@ -4,13 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception { //Styr vem som når vad
         
         http
         .authorizeHttpRequests((request) -> request
@@ -22,5 +26,21 @@ public class SecurityConfig {
         .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user1 = User.withDefaultPasswordEncoder()
+        .username("love")
+        .password("love")
+        .build();
+
+         UserDetails user2 = User.withDefaultPasswordEncoder()
+        .username("johan")
+        .password("johan")
+        .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
