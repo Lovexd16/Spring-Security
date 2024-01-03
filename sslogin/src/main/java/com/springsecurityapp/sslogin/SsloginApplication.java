2065c50dd1da694881e65de30b7711c73a3408aa
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-01-03 18:04:51 +0100

    mini app del 1

---
+++ sslogin/src/main/java/com/springsecurityapp/sslogin/SsloginApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurityapp.sslogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsloginApplication.class, args);
+	}
+
+}
---
+++ sslogin/src/test/java/com/springsecurityapp/sslogin/SsloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurityapp.sslogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
